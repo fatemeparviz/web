7c0d08dcdcb3f0499d2c49f99e08cfa98c680181
Fateme Parviz <fatemeprz1999@gmail.com>
2022-05-08 01:31:24 +0430

    search  component upgraded

--- src/component/Search.js
+++ src/component/Search.js
@@ -4,11 +4,26 @@ import Styles from "./Search.module.css"
 import React, { Component } from 'react';
 
 class Search extends Component {
+    constructor(){
+        super()
+        this.state={
+            search:""
+        }
+    }
+     ChangeHandeler=(event)=>{
+            this.setState({
+                search:event.target.value
+            })
+        }
+    
     render() {
         return (
             <div className={Styles.container}>
             <p>Search What You Want</p>
-            <input placeholder='Search ...'/>
+            <div className={Styles.search}>
+            <input type="text" value={this.state.text} onChange={this.ChangeHandeler} placeholder='Search ...'/>
+            <span>{this.state.search}</span>
+            </div>
         </div>
         );
     }
