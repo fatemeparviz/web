3b606b3fa177baabc815d2a9b9afdaa5f293595e
Fateme Parviz <fatemeprz1999@gmail.com>
2022-05-08 01:06:26 +0430

    arrow bottons were added for products and we can control the number of products with them

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,15 +1,50 @@
 import React, { Component } from 'react';
 import Styles from "./Card.module.css"
+import up from "../images/circle-arrow-up-solid.svg"
+import down from "../images/circle-arrow-down-solid.svg"
+import { click } from '@testing-library/user-event/dist/click';
+
 
 class Card extends Component {
     
+    constructor(){
+        super()
+        this.state={
+            counter:0
+        }
+    }
+
+    clickUp=()=>{
+        this.setState((prevState)=>({
+            counter:prevState.counter + 1,
+            // counter: prevState.target.value+1
+            
+        }))
+    }
+
+    clickDown=()=>{
+        if(this.state.counter >= 1){
+            this.setState((prevState)=>({
+                counter:prevState.counter -1
+               
+            }))
+        }
+        
+    }
+
     render() {
         const {image, name , price} = this.props
+        const {counter}=this.state
         return (
             <div className={Styles.Card}>
                 <img src={image}/>
                 <h3>{name}</h3>
                 <p>{price}</p>
+                <div className={Styles.counter} >
+                    <img src={up} alt="arrow" onClick={this.clickUp} />
+                    <span>{counter}</span>
+                    <img src={down} alt="arrow" className={counter ? "" : Styles.deactive } onClick={this.clickDown} />
+                </div>
             </div>
         );
     }
